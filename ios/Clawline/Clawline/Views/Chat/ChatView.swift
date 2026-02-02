a4404ae5bbc6ccf3102a2d03d0d4fe366a901c5d
Mike Manzano <mike@clicketyclacks.co>
2026-02-02 15:45:59 -0800

    Revert "Recompute keyboard inset on foreground"
    This reverts commit 59475b55c45500ab4dbfe531f92635d8ebf8da7f.

--- ios/Clawline/Clawline/Views/Chat/ChatView.swift
+++ ios/Clawline/Clawline/Views/Chat/ChatView.swift
@@ -820,12 +820,6 @@ private final class KeyboardLayoutGuideObserverView: UIView {
             name: UIResponder.keyboardWillChangeFrameNotification,
             object: nil
         )
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(appDidBecomeActive(_:)),
-            name: UIApplication.didBecomeActiveNotification,
-            object: nil
-        )
     }
 
     @available(*, unavailable)
@@ -835,32 +829,6 @@ private final class KeyboardLayoutGuideObserverView: UIView {
 
     deinit {
         NotificationCenter.default.removeObserver(self)
-    }
-
-    @objc private func appDidBecomeActive(_ notification: Notification) {
-        guard let window else { return }
-        window.layoutIfNeeded()
-        layoutIfNeeded()
-        let guideFrame = keyboardLayoutGuide.layoutFrame
-        let frameInWindow = convert(guideFrame, to: window)
-        let windowBounds = window.bounds
-        let widthDelta = windowBounds.width - frameInWindow.width
-        let isFloating = widthDelta > 1
-            || frameInWindow.minX > 1
-            || frameInWindow.maxX < windowBounds.maxX - 1
-        let height: CGFloat
-        if isFloating {
-            height = 0
-        } else {
-            height = max(0, windowBounds.maxY - frameInWindow.minY)
-        }
-        NSLog(
-            "[KBTIMING] keyboardFrameChanged foreground frame=%@ win=%@ floating=%d",
-            NSCoder.string(for: frameInWindow),
-            NSCoder.string(for: windowBounds),
-            isFloating ? 1 : 0
-        )
-        onChange?(height, lastDuration, lastCurve)
     }
 
     @objc private func keyboardFrameChanged(_ notification: Notification) {
